c49d696395d7537b01ca10bc0542fcdd50b1b170
Awesxme <manasvi.d007@gmail.com>
2026-05-05 18:05:28 +0000

    removed InitSpace trait

--- programs/prediction-clob/src/quantities.rs
+++ programs/prediction-clob/src/quantities.rs
@@ -3,15 +3,15 @@ use bytemuck::{Pod, Zeroable};
 
 // smallest unit of price movement
 #[repr(C)]
-#[derive(Default, Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, InitSpace, Pod, Zeroable)]
+#[derive(Default, Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Pod, Zeroable)]
 pub struct Ticks(pub u64);
 
 // smallest unit of outcome token
 #[repr(C)]
-#[derive(Default, Clone, Copy, Debug, PartialEq, Eq, InitSpace, Pod, Zeroable)]
+#[derive(Default, Clone, Copy, Debug, PartialEq, Eq, Pod, Zeroable)]
 pub struct BaseLots(pub u64);
 
 // smallest unit of collateral
 #[repr(C)]
-#[derive(Default, Clone, Copy, Debug, PartialEq, Eq, InitSpace, Pod, Zeroable)]
+#[derive(Default, Clone, Copy, Debug, PartialEq, Eq, Pod, Zeroable)]
 pub struct QuoteLots(pub u64);
